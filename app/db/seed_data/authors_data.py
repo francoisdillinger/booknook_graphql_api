5609db9c3b409c673d9140faa0c77dda97d3d3ea
francoisdillinger <elmojester@gmail.com>
2023-07-17 19:46:46 -0500

    Removing ID's from Author dummy data

--- app/db/seed_data/authors_data.py
+++ app/db/seed_data/authors_data.py
@@ -1,12 +1,27 @@
 authors_data = [
-  {"id": 1, "author_first_name": "George", "author_last_name":"Orwell" },
-  {"id": 2, "author_first_name": "Virginia", "author_last_name":"Woolf" },
-  {"id": 3, "author_first_name": "Ernest", "author_last_name":"Hemingway" },
-  {"id": 4, "author_first_name": "James", "author_last_name":"Joyce" },
-  {"id": 5, "author_first_name": "Emily", "author_last_name":"Bronte" },
-  {"id": 6, "author_first_name": "Jane", "author_last_name":"Austen" },
-  {"id": 7, "author_first_name": "Charles", "author_last_name":"Dickens" },
-  {"id": 8, "author_first_name": "Mark", "author_last_name":"Twain" },
-  {"id": 9, "author_first_name": "Harper", "author_last_name":"Lee" },
-  {"id": 10, "author_first_name": "J.D.", "author_last_name":"Salinger" }
+  { "author_first_name": "George", "author_last_name":"Orwell" },
+  { "author_first_name": "Virginia", "author_last_name":"Woolf" },
+  { "author_first_name": "Ernest", "author_last_name":"Hemingway" },
+  { "author_first_name": "James", "author_last_name":"Joyce" },
+  { "author_first_name": "Emily", "author_last_name":"Bronte" },
+  { "author_first_name": "Jane", "author_last_name":"Austen" },
+  { "author_first_name": "Charles", "author_last_name":"Dickens" },
+  { "author_first_name": "Mark", "author_last_name":"Twain" },
+  { "author_first_name": "Harper", "author_last_name":"Lee" },
+  { "author_first_name": "J.D.", "author_last_name":"Salinger" }
 ]
+
+
+
+# authors_data = [
+#   {"id": 1, "author_first_name": "George", "author_last_name":"Orwell" },
+#   {"id": 2, "author_first_name": "Virginia", "author_last_name":"Woolf" },
+#   {"id": 3, "author_first_name": "Ernest", "author_last_name":"Hemingway" },
+#   {"id": 4, "author_first_name": "James", "author_last_name":"Joyce" },
+#   {"id": 5, "author_first_name": "Emily", "author_last_name":"Bronte" },
+#   {"id": 6, "author_first_name": "Jane", "author_last_name":"Austen" },
+#   {"id": 7, "author_first_name": "Charles", "author_last_name":"Dickens" },
+#   {"id": 8, "author_first_name": "Mark", "author_last_name":"Twain" },
+#   {"id": 9, "author_first_name": "Harper", "author_last_name":"Lee" },
+#   {"id": 10, "author_first_name": "J.D.", "author_last_name":"Salinger" }
+# ]
