1380e5fa26187e045b84c99190f68ab5950bdbf8
francoisdillinger <elmojester@gmail.com>
2023-07-18 19:20:00 -0500

    Adding new root-level-resolvers for categories

--- app/graphQL/queries.py
+++ app/graphQL/queries.py
@@ -1,13 +1,16 @@
 from graphene import ObjectType, List
-from app.graphQL.types import UserObject, AuthorObject, BookObject
+from app.graphQL.types import UserObject, AuthorObject, BookObject, CategoryObject, BookCategoriesObject
 from app.db.database import db
-from app.db.models import User, Author, Book
+from app.db.models import User, Author, Book, Categories, BookCategories
 
 
 class Query(ObjectType):
     users = List(UserObject)
     authors = List(AuthorObject)
     books = List(BookObject)
+    categories = List(CategoryObject)
+    book_categories = List(BookCategoriesObject)
+
 
     # Root-Level Resolvers
     def resolve_users(root, info):
@@ -18,3 +21,9 @@ class Query(ObjectType):
     
     def resolve_books(root, info):
         return db.session.query(Book).all()
+    
+    def resolve_categories(root, info):
+        return db.session.query(Categories).all()
+    
+    def resolve_book_categories(root, info):
+        return db.session.query(BookCategories).all()
