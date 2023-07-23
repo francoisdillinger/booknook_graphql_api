8a5fd53578230ce6f8a790e4466352472f2fe981
francoisdillinger <elmojester@gmail.com>
2023-07-22 21:13:01 -0500

    Modifying DB to add initial Review data

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from app.utils.utils import add_initial_user_data, add_initial_author_data, add_initial_book_data, prepare_database, add_initial_category_data, add_initial_book_categories_data
-from app.db.models import Base, User, Author, Book, Categories, BookCategories
+from app.utils.utils import add_initial_user_data, add_initial_author_data, add_initial_book_data, add_initial_review_data, prepare_database, add_initial_category_data, add_initial_book_categories_data
+from app.db.models import Base, User, Author, Book, Review, Categories, BookCategories
 from app.db.seed_data.users_data import users_data
 from app.db.seed_data.authors_data import authors_data
 from app.db.seed_data.books_data import books_data
+from app.db.seed_data.book_reviews_data import reviews_data
 from app.db.seed_data.category_data import category_data
 from app.db.seed_data.book_category_data import book_category_data
 
@@ -16,5 +17,6 @@ def create_database(app):
         add_initial_user_data(User, db, users_data)
         add_initial_author_data(Author, db, authors_data)
         add_initial_book_data(Book, db, books_data)
+        add_initial_review_data(Review, db, reviews_data)
         add_initial_category_data(Categories, db, category_data)
         add_initial_book_categories_data(BookCategories, db, book_category_data)    
