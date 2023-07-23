5b1fbcf859c7ebeee91ae22c38324498604edbe4
francoisdillinger <elmojester@gmail.com>
2023-07-22 21:11:41 -0500

    Added additional users to Users_data

--- app/db/seed_data/users_data.py
+++ app/db/seed_data/users_data.py
@@ -1,3 +1,6 @@
 users_data = [
     { "user_name": "fuckinadmin", "password": "admin", "email": "admin@admin.com", "first_name": "Juicy", "last_name": "Jay", "role": "admin" },
+    { "user_name": "user1", "password": "admin", "email": "user1@admin.com", "first_name": "John", "last_name": "Doe", "role": "user" },
+    { "user_name": "user2", "password": "admin", "email": "user2@admin.com", "first_name": "Jane", "last_name": "Doe", "role": "user" },
+    { "user_name": "user3", "password": "admin", "email": "user3@admin.com", "first_name": "James", "last_name": "Doe", "role": "user" },
 ]
