935e24aff7df283a7ef4d5ccdf0f083433aefc13
francoisdillinger <elmojester@gmail.com>
2023-08-14 23:24:30 -0500

    Cleaning and importing verify_password function

--- app/graphQL/user/mutations.py
+++ app/graphQL/user/mutations.py
@@ -2,7 +2,7 @@ from graphene import Mutation, String, Int, List, Field
 from graphql import GraphQLError
 from app.db.models import User
 from app.db.database import db
-from app.utils.utils import generate_token, ph, VerifyMismatchError
+from app.utils.utils import generate_token, verify_password
 
 class LoginUser(Mutation):
     class Arguments:
@@ -17,10 +17,7 @@ class LoginUser(Mutation):
         if not user:
             raise GraphQLError('Invalid Email or password.')
         
-        try:
-            ph.verify(user.password, password)
-        except VerifyMismatchError:
-            raise GraphQLError('Invalid Email or password.')
+        verify_password(user.password, password)
         
         # token = ''.join(choices('abcdefghijklmnopqrstuvwxyz1234567890', k=10))
         token = generate_token(email)
