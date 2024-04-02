f53f095de0fec21e546e3e212170c5c2b7387224
francoisdillinger <elmojester@gmail.com>
2024-04-01 22:18:53 -0500

    Update data types in models.py

--- app/db/models.py
+++ app/db/models.py
@@ -27,7 +27,8 @@ class User(Base):
 class Author(Base):
     __tablename__ = 'authors'
 
-    id = Column(UUID, primary_key=True)
+    # id = Column(UUID, primary_key=True)
+    id = Column(Integer, primary_key=True)
     author_first_name = Column(String)
     author_last_name = Column(String)
 
@@ -45,7 +46,8 @@ class Book(Base):
     description = Column(String)
     inventory_count = Column(Integer)
     isbn = Column(String)
-    author_id = Column(UUID, ForeignKey('authors.id'))
+    # author_id = Column(UUID, ForeignKey('authors.id'))
+    author_id = Column(Integer, ForeignKey('authors.id'))
     
     # Relations
     author = relationship('Author', back_populates='books')
