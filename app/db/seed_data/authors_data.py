0ffe4d31b7a57a763ed5a8b3efe42e2539faf837
francoisdillinger <elmojester@gmail.com>
2024-04-03 14:52:33 -0500

    Update authors_data with new authors

--- app/db/seed_data/authors_data.py
+++ app/db/seed_data/authors_data.py
@@ -1,15 +1,30 @@
 authors_data = [
-  { "author_first_name": "George", "author_last_name":"Orwell" },
-  { "author_first_name": "Virginia", "author_last_name":"Woolf" },
-  { "author_first_name": "Ernest", "author_last_name":"Hemingway" },
-  { "author_first_name": "James", "author_last_name":"Joyce" },
-  { "author_first_name": "Emily", "author_last_name":"Bronte" },
-  { "author_first_name": "Jane", "author_last_name":"Austen" },
-  { "author_first_name": "Charles", "author_last_name":"Dickens" },
-  { "author_first_name": "Mark", "author_last_name":"Twain" },
-  { "author_first_name": "Harper", "author_last_name":"Lee" },
-  { "author_first_name": "J.D.", "author_last_name":"Salinger" }
+  {'id': '779fa00a-09d6-444e-9ce8-540e65ac5e19','author_first_name': 'Paulo','author_last_name': 'Coelho'},
+ {'id': '8283a8d6-e43b-4e48-8b4b-229e7a3f9a7f','author_first_name': 'Jane','author_last_name': 'Austen'},
+ {'id': '78f2351f-8786-45f6-8e8e-78d3dc51e9fa','author_first_name': 'Mark','author_last_name': 'Twain'},
+ {'id': 'a41e87a3-ed5a-432b-88e8-ac5d971f1c36','author_first_name': 'Virginia','author_last_name': 'Woolf'},
+ {'id': '19730510-9a97-4ce9-a39d-506bb7ff1a8f','author_first_name': 'Leo','author_last_name': 'Tolstoy'},
+ {'id': 'cc198234-0a02-4299-bd2f-fdba9edfbbf8','author_first_name': 'Fyodor','author_last_name': 'Dostoevsky'},
+ {'id': 'ae4b05b4-5e7b-44dc-9851-e8be1c36a79d','author_first_name': 'Ernest','author_last_name': 'Hemingway'},
+ {'id': 'b79427dc-0222-4547-8392-07cedf7a3e74','author_first_name': 'J.R.R.','author_last_name': 'Tolkien'},
+ {'id': '2c7320aa-327a-4d3f-bd77-67fd4e9415ce','author_first_name': 'George','author_last_name': 'Orwell'},
+ {'id': '9e668a21-9cd9-4c4f-aa01-76993332fec5','author_first_name': 'William','author_last_name': 'Faulkner'},
+ 
 ]
+
+
+# authors_data = [
+#   { "author_first_name": "George", "author_last_name":"Orwell" },
+#   { "author_first_name": "Virginia", "author_last_name":"Woolf" },
+#   { "author_first_name": "Ernest", "author_last_name":"Hemingway" },
+#   { "author_first_name": "James", "author_last_name":"Joyce" },
+#   { "author_first_name": "Emily", "author_last_name":"Bronte" },
+#   { "author_first_name": "Jane", "author_last_name":"Austen" },
+#   { "author_first_name": "Charles", "author_last_name":"Dickens" },
+#   { "author_first_name": "Mark", "author_last_name":"Twain" },
+#   { "author_first_name": "Harper", "author_last_name":"Lee" },
+#   { "author_first_name": "J.D.", "author_last_name":"Salinger" }
+# ]
 
 
 
