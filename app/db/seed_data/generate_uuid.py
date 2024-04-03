b4a2e10735b2970ac40218ce86d72183a9fb03f1
francoisdillinger <elmojester@gmail.com>
2024-04-03 16:20:50 -0500

    Add user_id to users_data in generate_uuid.py

--- app/db/seed_data/generate_uuid.py
+++ app/db/seed_data/generate_uuid.py
@@ -1,28 +1,43 @@
 import json
 import uuid
 from books_data import books_data
+from users_data import users_data
+# def add_book_id(book_data):
+#     book_data["book_id"] = str(uuid.uuid4())
+#     return book_data
 
-def add_book_id(book_data):
-    book_data["book_id"] = str(uuid.uuid4())
-    return book_data
+# def generate_python_code(books_data):
+#     python_code = "books_data = [\n"
+#     for book in books_data:
+#         python_code += "    " + str(book) + ",\n"
+#     python_code += "]\n"
+#     return python_code
 
-def generate_python_code(books_data):
-    python_code = "books_data = [\n"
-    for book in books_data:
-        python_code += "    " + str(book) + ",\n"
-    python_code += "]\n"
-    return python_code
+# def main():
+#     # Add book_id to each book
+#     modified_books_data = [add_book_id(book) for book in books_data]
+    
+#     # Generate Python code
+#     python_code = generate_python_code(modified_books_data)
+    
+#     # Write Python code to a new file
+#     with open("book_data.py", "w") as file:
+#         file.write(python_code)
+
+
+def add_user_id(user):
+    user["user_id"] = str(uuid.uuid4())
+    return user
 
 def main():
-    # Add book_id to each book
-    modified_books_data = [add_book_id(book) for book in books_data]
-    
-    # Generate Python code
-    python_code = generate_python_code(modified_books_data)
-    
-    # Write Python code to a new file
-    with open("book_data.py", "w") as file:
-        file.write(python_code)
+    modified_users = [add_user_id(user) for user in users_data]
+
+    with open("modified_users.py", "w") as file:
+        file.write("users = [\n")
+        for user in modified_users:
+            file.write("    " + str(user) + ",\n")
+        file.write("]\n")
+
 
 if __name__ == "__main__":
     main()
