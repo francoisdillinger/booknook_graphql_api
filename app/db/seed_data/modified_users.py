2f98032cb01436d193a67cb94286c45d8131cb4e
francoisdillinger <elmojester@gmail.com>
2024-04-03 16:20:45 -0500

    Add modified_users.py with initial user data

---
+++ app/db/seed_data/modified_users.py
@@ -0,0 +1,13 @@
+users = [
+    {'user_name': 'fuckinadmin', 'password': 'admin', 'email': 'admin@admin.com', 'first_name': 'Juicy', 'last_name': 'Jay', 'role': 'admin', 'user_id': '46f16466-6425-41ca-8c17-7ee79a20b0e7'},
+    {'user_name': 'booklover1', 'password': 'admin', 'email': 'booklover1@admin.com', 'first_name': 'John', 'last_name': 'Doe', 'role': 'user', 'user_id': '3e4992a5-bde8-4df2-ae06-051d0b7a1805'},
+    {'user_name': 'reader123', 'password': 'admin', 'email': 'reader123@admin.com', 'first_name': 'Jane', 'last_name': 'Smith', 'role': 'user', 'user_id': '9c1b9d38-b2df-4a79-8091-8ef8fa821c88'},
+    {'user_name': 'bibliophile', 'password': 'admin', 'email': 'bibliophile22@admin.com', 'first_name': 'Michael', 'last_name': 'Johnson', 'role': 'user', 'user_id': '8410ebc7-bddd-4cff-a651-24b74f193192'},
+    {'user_name': 'bookworm', 'password': 'admin', 'email': 'bookworm4eva@admin.com', 'first_name': 'Emily', 'last_name': 'Davis', 'role': 'user', 'user_id': '550de341-e5c4-486f-b087-5ef77ff3a72b'},
+    {'user_name': 'avidreader7', 'password': 'admin', 'email': 'avidreader7@admin.com', 'first_name': 'David', 'last_name': 'Anderson', 'role': 'user', 'user_id': '7c7a29c9-a713-440b-9b85-eb5033f02641'},
+    {'user_name': 'bookishgal', 'password': 'admin', 'email': 'bookishgal@admin.com', 'first_name': 'Sarah', 'last_name': 'Wilson', 'role': 'user', 'user_id': '671b88b5-2cb1-43ff-9b2d-c19c6e319ef7'},
+    {'user_name': 'litlover88', 'password': 'admin', 'email': 'litlover88@admin.com', 'first_name': 'Robert', 'last_name': 'Brown', 'role': 'user', 'user_id': '7bb8095f-c1a8-4307-b74f-2affe40bae4b'},
+    {'user_name': 'jenreads', 'password': 'admin', 'email': 'jenniferreads@admin.com', 'first_name': 'Jennifer', 'last_name': 'Lee', 'role': 'user', 'user_id': '85e505f3-d55e-45cb-8d38-f91ae9164d17'},
+    {'user_name': 'iambook', 'password': 'admin', 'email': 'williambookworm@admin.com', 'first_name': 'William', 'last_name': 'Taylor', 'role': 'user', 'user_id': 'de223b1f-28ef-4921-b846-0613747e1cee'},
+    {'user_name': 'lovesbooks', 'password': 'admin', 'email': 'lauralovesbooks@admin.com', 'first_name': 'Laura', 'last_name': 'Miller', 'role': 'user', 'user_id': 'ca1f6b8e-fa79-4d48-8cbc-dd0ef439327e'},
+]
