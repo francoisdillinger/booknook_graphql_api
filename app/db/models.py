efe625a92911564d4491ffffb62a8b69bee985f0
francoisdillinger <elmojester@gmail.com>
2024-04-04 23:07:10 -0500

    Update UUID columns in models.py

--- app/db/models.py
+++ app/db/models.py
@@ -145,9 +145,9 @@ class WishList(Base):
     # id = Column(Integer, primary_key=True, autoincrement=True)
     # user_id = Column(Integer, ForeignKey('users.id'))
     # book_id = Column(Integer, ForeignKey('books.id'))
-    id = Column(UUID, primary_key=True)
-    user_id = Column(UUID, ForeignKey('users.id'))
-    book_id = Column(UUID, ForeignKey('books.id'))
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'))
+    book_id = Column(UUID(as_uuid=True), ForeignKey('books.id'))
     
     # Relations
     user = relationship('User', back_populates='wish_list')
